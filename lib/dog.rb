32fedecdd63534e2588a6a143612582060c51668
Aida Abdikulova <abdikulova@gmail.com>
2019-09-12 00:39:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 class Dog
 
-attr_accessor :name, :breed, :id
+attr_accessor :name, :breed
 
-def initialize(name:,breed:,id=nil)
+def initialize(name:,breed:)
   @name = name
   @breed = breed
-  @id = id
 end
 
 end
