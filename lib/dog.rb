0f20de6455fccc784c426301f863447d59dd4c03
Aida Abdikulova <abdikulova@gmail.com>
2019-09-12 00:29:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,91 @@
+class Dog
+
+  attr_accessor :id,:name,:breed
+
+  def initialize(id:nil,name:,breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name,breed)
+        VALUES (?,?)
+        SQL
+      DB[:conn].execute(sql,self.name,self.breed)
+      @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+      self
+    end
+  end
+
+  def self.create(name:,breed:)
+    new_dog = Dog.new(name:name,breed:breed)
+    new_dog.save
+  end
+
+  def self.new_from_db(row)
+    new_dog = Dog.new(id:row[0],name:row[1],breed:row[2])
+    new_dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      SQL
+    DB[:conn].execute(sql,id).map {|row| self.new_from_db(row)}.first
+  end
+
+  def self.find_or_create_by(name:,breed:)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+    check = DB[:conn].execute(sql,name,breed)
+    check.any? ? (self.new_from_db(check[0])) : (self.create(name:name,breed:breed))
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      SQL
+    DB[:conn].execute(sql,name).map {|row| self.new_from_db(row)}.first
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+      SQL
+    DB[:conn].execute(sql,self.name,self.breed,self.id)
+    self
+  end
+
+end
